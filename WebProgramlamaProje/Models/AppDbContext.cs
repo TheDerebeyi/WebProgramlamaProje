3097e5ff87e5812be94c88d2fee4dbd87a38181a
Mert Kahraman <73400722+TheDerebeyi@users.noreply.github.com>
2022-12-07 19:34:16 +0300

    Mssql ve EF Core Tools eklendi

---
+++ WebProgramlamaProje/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebProgramlamaProje.Models
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=App,Trusted_Connection=True");
+
+        }
+    }
+}
